6d1731b916e00adaa8bf873e0b9d64ad4897c339
Samme <samme4life@gmail.com>
2018-09-11 05:02:13 +1000

    Sec 3 Lec 49 Outputting a list of Recipes with ngFor

--- ng6-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ng6-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a Test',
+      'https://www.slenderkitchen.com/sites/default/themes/slender_kitchen_theme/images/slide/slenderkitchen-meal-plan-recipe-c.png')
+  ];
 
   constructor() { }
 
---
+++ ng6-complete-guide/src/app/recipes/recipe.model.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by sameera on 7/09/2018.
+ */
+export class Recipe {
+  public name: string;
+  public description: string;
+  public imagePath: string;
+
+  constructor(name: string, description: string, imagePath: string) {
+    this.name = name;
+    this.description = description;
+    this.imagePath = imagePath;
+  }
+}
