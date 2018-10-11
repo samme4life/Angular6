285630a81977625941569ed5d4b83622f6f2ec4d
Samme <samme4life@gmail.com>
2018-10-11 17:18:07 +1000

    Sec 7 Lec 87 Using the Renderer to build a Better Attribute Directive

--- directives-start/src/app/app.module.ts
+++ directives-start/src/app/app.module.ts
@@ -4,12 +4,14 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
-import {BasicHighlightDirective} from './basic-highlight/basic-highlight.directive';
+import {BasicHighlightDirective} from './directives/basic-highlight/basic-highlight.directive';
+import {BetterHighlightDirective} from './directives/better-highlight/better-highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BasicHighlightDirective
+    BasicHighlightDirective,
+    BetterHighlightDirective
   ],
   imports: [
     BrowserModule,
