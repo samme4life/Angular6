6ff2af73aa04cc5dd1e51d4ad265b7d8d1492be4
Samme <samme4life@gmail.com>
2018-10-11 12:07:49 +1000

    Sec 6 Lec 79 Passing Recipe Data with property binding

--- ng6-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ng6-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,8 +11,8 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a Test',
       'https://familynutrition.co.uk/wp-content/uploads/2014/06/Recipe-icon.png'),
-    new Recipe('A Test Recipe', 'This is simply a Test',
-      'https://familynutrition.co.uk/wp-content/uploads/2014/06/Recipe-icon.png')
+    new Recipe('Another Test Recipe', 'This is also a Test',
+      'http://icons.iconarchive.com/icons/lemon-liu/recipes/256/recipe-noodles-pasta-icon.png')
   ];
 
   constructor() { }
