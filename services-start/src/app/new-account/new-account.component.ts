544cb7bdf4403f9bd65613dedc6fca021efa765b
Samme <samme4life@gmail.com>
2018-10-12 14:11:58 +1000

    Sec 9 Lec 99 Injecting the Logging Service into Components

--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import {LoggingService} from '../shared/logging.service';
 
 @Component({
   selector: 'app-new-account',
@@ -13,6 +14,7 @@ export class NewAccountComponent {
       name: accountName,
       status: accountStatus
     });
-    console.log('A server status changed, new status: ' + accountStatus);
+    const loggingService = new LoggingService();
+    loggingService.logStatusChange('halloooo A server status changed, new status: ' + accountStatus);
   }
 }
