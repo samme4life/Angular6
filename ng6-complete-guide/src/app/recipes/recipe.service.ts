85f15e2dd333d2a2b4e387b57235c6a8f2aa58d5
Samme <samme4life@gmail.com>
2018-10-15 11:25:34 +1000

    Sec 10 Lec 112 Adding Ingredients to Recipes

--- ng6-complete-guide/src/app/recipes/recipe.service.ts
+++ ng6-complete-guide/src/app/recipes/recipe.service.ts
@@ -1,15 +1,28 @@
 import {Recipe} from "./recipe.model";
 import {EventEmitter} from "@angular/core";
+import {Ingredient} from "../shared/ingredient.model";
 
 export  class RecipeService {
 
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a Test',
-      'https://familynutrition.co.uk/wp-content/uploads/2014/06/Recipe-icon.png'),
-    new Recipe('Another Test Recipe', 'This is also a Test',
-      'http://icons.iconarchive.com/icons/lemon-liu/recipes/256/recipe-noodles-pasta-icon.png')
+    new Recipe(
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Wiener-Schnitzel03.jpg/1280px-Wiener-Schnitzel03.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe(
+      'Big Fat Burger',
+      'What else you need to say?',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/RedDot_Burger.jpg/1920px-RedDot_Burger.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ])
   ];
 
   getRecipes() {
