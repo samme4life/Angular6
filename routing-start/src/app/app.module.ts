38326812d76c618c1312ddbd6a8b3107d153ffe4
Samme <samme4life@gmail.com>
2018-10-16 16:09:35 +1000

    Sec 11 Lec 137 Protecting Routes with canActivate

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -13,19 +13,9 @@ import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import {RouterModule, Routes} from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent},
-  { path: 'users', component: UsersComponent, children: [
-      { path: ':id/:name', component: UserComponent}
-    ]},
-  { path: 'servers', component: ServersComponent, children: [
-      { path: ':id', component: ServerComponent},
-      { path: ':id/edit', component: EditServerComponent}
-    ]},
-  { path: 'not-found', component: PageNotFoundComponent},
-  { path: '**', redirectTo: '/not-found'}
-];
+import {AppRoutingModule} from './app-routing.module';
+import {AuthService} from './auth-service';
+import {AuthGuard} from './auth-guard.service';
 
 @NgModule({
   declarations: [
@@ -42,9 +32,9 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
-  providers: [ServersService],
+  providers: [ServersService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
