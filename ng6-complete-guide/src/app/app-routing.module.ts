7173a8b1c0fbbe4569f2dac4c95e4ec55d8ba62f
Samme <samme4life@gmail.com>
2018-10-18 12:14:26 +1000

    Sec 12 Lec 152 Configuring Route Parameters

--- ng6-complete-guide/src/app/app-routing.module.ts
+++ ng6-complete-guide/src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {RecipeComponent} from './recipes/recipe.component';
 import {ShoppingListComponent} from './shopping-list/shopping-list.component';
+import {RecipeStartComponent} from './recipes/recipe-start/recipe-start.component';
+import {RecipeDetailsComponent} from './recipes/recipe-details/recipe-details.component';
 
 const appRoutes: Routes = [
   {path: '', redirectTo: '/recipes', pathMatch: 'full'},
-  {path: 'recipes', component: RecipeComponent},
+  {path: 'recipes', component: RecipeComponent, children: [
+      {path: '', component: RecipeStartComponent},
+      {path: ':id', component: RecipeDetailsComponent}
+    ]},
   {path: 'shopping-list', component: ShoppingListComponent},
 ]
 
--- ng6-complete-guide/src/app/recipes/recipe-details/recipe-details.component.ts
+++ ng6-complete-guide/src/app/recipes/recipe-details/recipe-details.component.ts
@@ -1,6 +1,7 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Recipe} from "../recipe.model";
 import {RecipeService} from "../recipe.service";
+import {ActivatedRoute, Params} from '@angular/router';
 
 @Component({
   selector: 'app-recipe-details',
@@ -8,11 +9,20 @@ import {RecipeService} from "../recipe.service";
   styleUrls: ['./recipe-details.component.css']
 })
 export class RecipeDetailsComponent implements OnInit {
-  @Input() recipe: Recipe;
+  recipe: Recipe;
+  id: number;
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService,
+              private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.activatedRoute.params
+      .subscribe(
+        (params: Params) => {
+          this.id = +params['id'];
+          this.recipe = this.recipeService.getRecipe(this.id);
+        }
+      )
   }
 
   onAddToShoppingList() {
