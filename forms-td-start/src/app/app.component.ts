fe62be21fbcd45ab94b88c2c0c4beb4659e7226c
Samme <samme4life@gmail.com>
2018-10-22 14:36:27 +1000

    Sec 15 Lec 176 TD: Creating the Form and Registering the Controls Lec 177 TD: Submitting and Using the form Lec 178 TD: Understanding Form state Lec 179 TD: Accessing the Form with @ViewChild

---
+++ forms-td-start/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  @ViewChild('f') signupForm: NgForm;
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
+
+  // onSubmit(form: NgForm) {
+  //   console.log('Submitted');
+  //   console.log(form);
+  // }
+
+  onSubmit(form: NgForm) {
+    console.log('Submitted');
+    console.log(this.signupForm);
+  }
+}
