18ded7b10e55d7c4e46efa23739b6a3693873fd9
Samme <samme4life@gmail.com>
2018-10-23 10:18:40 +1000

    Sec 15 Lec 189 Using Form Data forms-reactive-start project was added to the git

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -13,9 +13,35 @@ export class AppComponent {
   defaultUsername = 'akain';
   answer = '';
   genders = ['male', 'female', 'other'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: 'abc@gmail.com'
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    // this.signupForm.form.setValue({
+    //     userData: {
+    //       username: suggestedName,
+    //       email: 'abc@gmail.com'
+    //     },
+    //     secret: 'pet',
+    //     questionAnswer: '',
+    //     gender: 'male'
+    //   });
     this.signupForm.form.patchValue({
       userData: {
         username: suggestedName
@@ -28,8 +54,12 @@ export class AppComponent {
   //   console.log(form);
   // }
 
-  onSubmit(form: NgForm) {
-    console.log('Submitted');
-    console.log(this.signupForm);
+  onSubmit() {
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
   }
 }
