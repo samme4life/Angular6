d5865eca551ab38fbcd85b84edee999289aad196
Samme <samme4life@gmail.com>
2018-10-23 11:22:21 +1000

    Added sample project app files Sec 15 Lec 192 Reactive: Setup

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'forms-reactive-start';
+  genders = ['male', 'female'];
+  signupForm: FormGroup;
 }
