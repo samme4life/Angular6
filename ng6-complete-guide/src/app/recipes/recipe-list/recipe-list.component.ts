00b161beabded13e6fb027b8b25a22f22f8db29e
Samme <samme4life@gmail.com>
2018-10-25 15:38:33 +1000

    Sec 16 Lec 220 Submitting the Recipe Edit Form

--- ng6-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ng6-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -17,6 +17,12 @@ export class RecipeListComponent implements OnInit {
               private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.recipeService.recipesChanged
+      .subscribe(
+        (recipes: Recipe[]) => {
+          this.recipes = recipes;
+        }
+      );
     this.recipes = this.recipeService.getRecipes();
   }
 
